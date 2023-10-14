704785dda20e12ed8b59d65979041f54ba4389c5
n1bil <nabil.nadzor@gmail.com>
2023-10-14 18:31:38 +0200

    first push my keeper project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Note from "./components/Note";
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Note />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
